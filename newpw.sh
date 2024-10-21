4b2b24342fde8175074399e2a59e250ae645ab2f
zegfarce <zegfarce@protonmail.com>
2024-10-21 05:15:46 -0500

    Implement way to check for terminal, empty file, and closed fd without losing data
    - When any of these situations come up, reads from /dev/urandom as a source instead.
    - This is able to distinguish end-of-file events and nul bytes, too.
    	- This means /dev/zero and /dev/null behave differently!
    	  When I tested with /dev/zero, the program hangs..
    	  But /dev/null will cause the program to read from /dev/urandom instead.

--- newpw.sh
+++ newpw.sh
@@ -48,13 +48,43 @@ for param in "$@"; do
 	arg="$param"
 done
 
-set --
 
-# todo: check for EOF in fd 0 and account for it by using /dev/urandom
-# likely abusing `0<filename command [args]`
-if [ -t 0 ]; then
-	set -- "$defsrc"
-fi
 
-cat "$@" | tr -cd "$match" | head -c "$length"
+# big hack so shell scripting can achieve what was previously considered impossible
+# no additional shell or os capabilities required! no third file descriptor!
+# no fifo or temporary files! fully posix! kill me!
+
+# tries to read stdin.
+# if its a terminal, an empty file, or a closed fd,
+# then use the first argument as a path to a file to read instead
+stdin_or_file() {
+	# dont ask the terminal for data
+	if [ -t 0 ]; then
+		cat "$1"
+		exit
+	fi
+
+	# read and consume a character as octal
+	# no going back
+
+	# when a nul byte is read from fd 0, xargs complains about it..
+	# .. and then printf complains about invalid syntax..
+	# .. but it writes "000" anyway, so in the end it works out.
+	# if no bytes at all are sent, char becomes an empty string
+	char="$(dd bs=1 count=1 2>/dev/null | xargs -I {} printf "%03o" "'{}" 2>/dev/null)"
+	# if xargs -0 were in posix i could avoid one warning, oh well
+
+	# check for actual lack of data, not a nul byte!
+	if [ -z "$char" ]; then
+		cat "$1"
+	else # oops we got data
+		# its like it was never gone
+		printf "%s" "$(eval 'printf "'\\"$char"'"')"
+
+		# catch closed fd 0 incase some smart-alec ran us with <&-
+		cat 2>/dev/null || cat "$1"
+	fi
+}
+
+stdin_or_file "$defsrc" | tr -cd "$match" | head -c "$length"
 printf "\n"
